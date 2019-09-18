8ba63381d27ed3600bcd9b93f26999c392e231fb
The Launch <daniel@thelaunch.tech>
2019-09-17 20:45:28 -0400

    boilerplate commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <header></header>
+        <article></article>
+        <footer></footer>
+      </Router>
+    )
+  }
+}
+
+export default App
